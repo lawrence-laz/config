d81bbcd2bee94e28d7e1d8e329a75dbd4e6cce82
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2022-02-28 11:13:50 +0200

    Update git-config.sh

--- git-config.sh
+++ git-config.sh
@@ -6,3 +6,6 @@ sudo apt-get install libsecret-1-0 libsecret-1-dev
 cd /usr/share/doc/git/contrib/credential/libsecret
 sudo make
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
+# Is the following command not needed when using libsecret?
+# git config --global credential.helper store
